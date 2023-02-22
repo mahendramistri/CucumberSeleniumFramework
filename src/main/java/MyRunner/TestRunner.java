354e8d434b1445ef948a5d8d997fc281433685a4
Mahendra <mahendramistri@gmail.com>
2023-02-22 16:10:37 +0530

    update project

--- src/main/java/MyRunner/TestRunner.java
+++ src/main/java/MyRunner/TestRunner.java
@@ -1,19 +1,22 @@
 package MyRunner;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 
 
-	@RunWith(Cucumber.class)
+
+
+@RunWith(Cucumber.class)
 	@CucumberOptions(
-			features = "/Users/NaveenKhunteta/Documents/workspace/FreeCrmBDDFramework/src/main/java/Features/taggedhooks.feature", //the path of the feature files
-			glue={"stepDefinitions"}, //the path of the step definition files
-			format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting
-			monochrome = true, //display the console output in a proper readable format
-			strict = true, //it will check if any step is not defined in step definition file
-			dryRun = false //to check the mapping is proper between feature file and step def file
-			//tags = {"~@SmokeTest" , "~@RegressionTest", "~@End2End"}			
+			features = "/home/dell/Documents/Projects/CucumberSeleniumFramework/src/main/java/Features/deals.feature", //the path of the feature files
+			glue={"/src/main/java/stepDefinitions"} //the path of the step definition files
+//			format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting
+//			monochrome = true, //display the console output in a proper readable format
+//			strict = true, //it will check if any step is not defined in step definition file
+//			dryRun = false //to check the mapping is proper between feature file and step def file
+//			//tags = {"~@SmokeTest" , "~@RegressionTest", "~@End2End"}
 			)
 	 
 	public class TestRunner {
