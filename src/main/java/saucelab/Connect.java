d9b3d484c3c86c6500cf9369188f1baeb6347f48
Mahendra <mahendramistri@gmail.com>
2023-03-02 13:07:05 +0530

    update auth

--- src/main/java/saucelab/Connect.java
+++ src/main/java/saucelab/Connect.java
@@ -17,15 +17,14 @@ import java.net.URL;
 
 public class Connect {
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
-
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
         capabilities.setPlatform(Platform.VISTA);
         Map<String, Object> sauceOptions = new HashMap<>();
         sauceOptions.put("build", "selenium-build-5APUU");
        // sauceOptions.setCapability("name", "<your test name>");
         capabilities.setCapability("sauce:options", sauceOptions);
-        // Create a RemoteWebDriver instance and connect to the local browser
-        WebDriver driver1 = new RemoteWebDriver(new URL("https://oauth-user-bef8-b187c151a0f6@ondemand.eu-central-1.saucelabs.com:443/wd/hub"), capabilities);
+        // Create a RemoteWebDriver instance and connect to the local browsermvn
+        WebDriver driver1 = new RemoteWebDriver(new URL("https://oauth-mahendramistri-b2316:b82ecf6f-d121-4c96-bef8-b187c151a0f6@ondemand.eu-central-1.saucelabs.com:443/wd/hub"), capabilities);
         // Perform the test using the driver instance
         driver1.get("https://mvnrepository.com/artifact/org.testng/testng");
         Thread.sleep(300);
